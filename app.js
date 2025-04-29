51b0c2f9068a56ff887cc3844e7b2f292ce60d99
annae <anna.romagosa@smartassets.ai>
2025-04-29 21:24:33 +0200

    base structure created

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+const express = require("express");
+const { default: routes } = require("./router");
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.json());
+
+const { cartsRoute, productsRoute } = routes;
+
+// API: products
+app.get(productsRoute, (req, res) => {});
+
+app.get(`${productsRoute}/:pid`, (req, res) => {});
+
+app.post(productsRoute, (req, res) => {});
+
+app.put(`${productsRoute}/:pid`, (req, res) => {});
+
+app.delete(`${productsRoute}/:pid`, (req, res) => {});
+
+// API: carts
+app.post(cartsRoute, (req, res) => {});
+
+app.get(`${cartsRoute}/:cid`, (req, res) => {});
+
+app.post(`${cartsRoute}/:cid/product/:pid`, (req, res) => {});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ router.js
@@ -0,0 +1,11 @@
+const baseRoute = "/api";
+
+const productsRoute = baseRoute.concat("/products");
+const cartsRoute = baseRoute.concat("/carts");
+
+const routes = {
+  productsRoute,
+  cartsRoute,
+};
+
+export default routes;
