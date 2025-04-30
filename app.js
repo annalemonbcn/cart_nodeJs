8833900ffcb78977b991230a4b707cc8f3d56a48
Anna Esteve <aesteve8@gmail.com>
2025-04-30 12:27:16 +0200

    Delivery 1 (#1)
    * base structure created
    
    * cartManager 1st approach
    
    * productsManager 1st approach
    
    * productsManager refined
    
    * productsManager correction
    
    * cartManager improved
    
    * productsManager cleaning
    
    * api products endpoint implemented
    
    * api carts endpoints implemented
    
    * small refactor
    
    ---------
    
    Co-authored-by: annae <anna.romagosa@smartassets.ai>

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const express = require("express");
+const path = require("path");
+
+const productsRoutes = require("./routes/products.routes");
+const cartsRoutes = require("./routes/carts.routes");
+
+const PORT = 8080;
+const app = express();
+app.use(express.json());
+
+app.use("/api/products", productsRoutes);
+app.use("/api/carts", cartsRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
