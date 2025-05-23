97bfb5d1af2a933348f69ae5ef284a391a0e2829
annae <anna.romagosa@smartassets.ai>
2025-05-23 16:52:52 +0200

    handlebars + socket.io integrated

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+
 const productsRoutes = require("./src/routes/products.routes");
 const cartsRoutes = require("./src/routes/carts.routes");
 
 const PORT = 8080;
 const app = express();
 
-app.use(express.json());
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/products", productsRoutes);
 app.use("/api/carts", cartsRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const http = require("http").createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(http);
+
+io.on("connection", (socket) => {
+  console.log("connected");
+
+  // socket.on("message", (data) => {
+  //   console.log("data", data);
+  // });
+});
+
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
