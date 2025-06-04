e750432a5ae45e5d50b8567ee7623c557cc263e0
annae <anna.romagosa@smartassets.ai>
2025-06-05 00:03:25 +0200

    connection to db created

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 import express from "express";
 import { engine } from "express-handlebars";
-import path from "path";
+import path, { dirname, join } from "path";
+import dotenv from "dotenv";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
-import { createServer } from "http";
+
+import { connectToDatabase } from "./src/config/db.js";
+
 import productsRoutes from "./src/routes/products.routes.js";
 import cartsRoutes from "./src/routes/carts.routes.js";
 import viewsRoutes from "./src/routes/views.routes.js";
 
-const PORT = 8080;
-const app = express();
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.MONGO_PORT;
+
+app.use(express.json());
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "src", "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/products", productsRoutes);
 app.use("/api/carts", cartsRoutes);
 app.use("/", viewsRoutes);
 
-const http = createServer(app);
+const startServer = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+  );
+};
 
-// const { Server } = require("socket.io");
-// const io = new Server(http);
-
-// io.on("connection", (socket) => {
-//   console.log("connected");
-// });
-
-http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+startServer();
---
+++ src/config/db.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const DB_NAME = process.env.MONGO_DB_NAME;
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, { dbName: DB_NAME });
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+const disconnectFromDatabase = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log("✅ Disconnected to MongoDB");
+  } catch (error) {
+    console.error("❌ MongoDB disconnection error:", error.message);
+    process.exit(1);
+  }
+};
+
+export { connectToDatabase, disconnectFromDatabase };
