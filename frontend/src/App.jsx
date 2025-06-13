786493952e46c90f0e96406c15bf63b0c94c9565
annae <anna.romagosa@smartassets.ai>
2025-06-13 18:03:22 +0200

    axios + tanstack query integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const App = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/api/products")
-      .then((res) => setProducts(res.data));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:8080/api/products")
+  //     .then((res) => setProducts(res.data));
+  // }, []);
 
-  useEffect(() => console.log("products", products), [products]);
+  // useEffect(() => console.log("products", products), [products]);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:8080/api/products");
+      return res.data;
+    },
+  });
+
+  console.log("data", data);
 
   return <div>Hey! Working with react</div>;
 };
