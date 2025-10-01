e0b76c029ca7f89e035ef66905c7cca184fb8cc0
annae <anna.romagosa@smartassets.ai>
2025-10-01 16:39:26 +0200

    sendgrid working

--- src/services/auth/auth.services.js
+++ src/services/auth/auth.services.js
@@ -9,7 +9,8 @@ import {
 } from "./validations.js";
 import { decodeToken, generateToken } from "#controllers/auth/utils.js";
 import { encryptPassword } from "#utils/bcrypt.js";
-import sgMail from "@sendgrid/mail";
+import { emailTemplates } from "#utils/emailtemplates/resetPassword.js";
+import sgMail from "#config/sendgrid/index.js";
 
 const registerUserService = (req) => {
   return new Promise((resolve) => {
@@ -41,82 +42,14 @@ const forgotPasswordService = async (email) => {
   const token = generateToken({ id: user._id, email: user.email });
   const resetURL = `${process.env.FRONTEND_URL}/reset-password?token=${token}&mode=reset`;
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-  await sgMail.send({
+  const msg = {
     to: user.email,
     from: "annalemonbcn.dev@gmail.com",
     subject: "Password Reset Request",
-    html: `
-    <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
-      <div style="max-width: 600px; margin: auto; padding: 20px; border: 1px solid #e0e0e0; border-radius: 8px;">
-        <h2 style="color: #8A33FD;">Reset Your Password</h2>
-        <p>Hello ${user.firstName || ""},</p>
-        <p>You are receiving this email because you (or someone else) requested a password reset for your account.</p>
-        
-        <div style="text-align: center; margin: 30px 0;">
-          <a href="${resetURL}" 
-             style="background-color: #8A33FD; color: white; padding: 12px 24px; text-decoration: none; border-radius: 8px; display: inline-block;">
-            Reset Password
-          </a>
-        </div>
-  
-        <p>If the button doesn’t work, copy and paste the following URL into your browser:</p>
-        <p style="word-break: break-all;"><a href="${resetURL}" style="color: #0D6EFD;">${resetURL}</a></p>
-  
-        <p>If you did not request a password reset, please ignore this email. Your account remains safe.</p>
-  
-        <hr style="border: none; border-top: 1px solid #e0e0e0; margin: 20px 0;">
-        <p style="font-size: 12px; color: #888;">© 2025 Anna Lemon BCN. All rights reserved.</p>
-      </div>
-    </div>
-    `,
-  });
+    html: emailTemplates.passwordResetTemplate(user, resetURL),
+  };
 
-  // const transporter = nodemailer.createTransport({
-  //   service: "SendGrid",
-  //   auth: {
-  //     user: "apikey",
-  //     pass: process.env.SENDGRID_API_KEY,
-  //   },
-  // });
-
-  // const mailOptions = {
-  //   from: "annalemonbcn.dev@gmail.com",
-  //   to: user.email,
-  //   subject: "Password Reset Request",
-  //   html: `
-  //   <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
-  //     <div style="max-width: 600px; margin: auto; padding: 20px; border: 1px solid #e0e0e0; border-radius: 8px;">
-  //       <h2 style="color: #8A33FD;">Reset Your Password</h2>
-  //       <p>Hello ${user.firstName || ""},</p>
-  //       <p>You are receiving this email because you (or someone else) requested a password reset for your account.</p>
-
-  //       <div style="text-align: center; margin: 30px 0;">
-  //         <a href="${resetURL}"
-  //            style="background-color: #8A33FD; color: white; padding: 12px 24px; text-decoration: none; border-radius: 8px; display: inline-block;">
-  //           Reset Password
-  //         </a>
-  //       </div>
-
-  //       <p>If the button doesn’t work, copy and paste the following URL into your browser:</p>
-  //       <p style="word-break: break-all;"><a href="${resetURL}" style="color: #8A33FD;">${resetURL}</a></p>
-
-  //       <p>If you did not request a password reset, please ignore this email. Your account remains safe.</p>
-
-  //       <hr style="border: none; border-top: 1px solid #e0e0e0; margin: 20px 0;">
-  //       <p style="font-size: 12px; color: #888;">© 2025 Anna Lemon BCN. All rights reserved.</p>
-  //     </div>
-  //   </div>
-  //   `,
-  // };
-
-  // try {
-  //   await transporter.sendMail(mailOptions);
-  // } catch (err) {
-  //   console.error("Error sending email:", err);
-  //   throw new InternalServerError("Failed to send email");
-  // }
+  await sgMail.send(msg);
 };
 
 const resetPasswordService = async (token, password) => {
