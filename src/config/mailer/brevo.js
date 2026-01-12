942f5945828cfbb82f0204d2c50e9f2b35e7d626
annalemonbcn <aesteve8@gmail.com>
2026-01-12 15:30:55 +0100

    brevo client configured

---
+++ src/config/mailer/brevo.js
@@ -0,0 +1,33 @@
+import "dotenv-flow/config";
+import Brevo from "@getbrevo/brevo";
+
+const apiInstance = new Brevo.TransactionalEmailsApi();
+
+const apiKey = apiInstance.authentications["apiKey"];
+if (!process.env.BREVO_API_KEY) {
+  throw new Error("BREVO_API_KEY is missing");
+}
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+const FROM_EMAIL = process.env.MAIL_FROM_EMAIL || "annalemonbcn.dev@gmail.com";
+const FROM_NAME = process.env.MAIL_FROM_NAME || "Anna Lemon BCN";
+
+const sendBrevoMail = async ({ to, templateId, dynamicTemplateData }) => {
+  console.log("to", to);
+  console.log("templateId", templateId);
+  console.log("dynamicTemplateData", dynamicTemplateData);
+
+  const email = {
+    to: [{ email: to }],
+    templateId: Number(templateId),
+    params: dynamicTemplateData,
+    sender: {
+      email: FROM_EMAIL,
+      name: FROM_NAME,
+    },
+  };
+
+  await apiInstance.sendTransacEmail(email);
+};
+
+export { sendBrevoMail };
